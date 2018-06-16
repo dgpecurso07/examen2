08b06bf0db502cd54dcdbcfadf5182bae7fc0272
dgpecurso07 <dgpe.curso.07@gmail.com>
2018-06-15 19:33:46 -0500

    hola2

--- src/test/java/mx/unam/dgpe/sample/controller/TestMyController.java
+++ src/test/java/mx/unam/dgpe/sample/controller/TestMyController.java
@@ -10,13 +10,13 @@ import static mx.unam.dgpe.sample.controller.RestUtil.*;
 public class TestMyController extends AbstractVerticle {
     private static final Logger logger = Logger.getLogger(TestMyController.class);
     
-    @Test
+    //@Test
     public void ok() throws Exception {
         //Se debe configurar ip y puerto para que test funcione correctamente
 
     	//String result = sendGet("http://192.168.1.145:8082/api/factorial?num=5");
         //logger.info(result);
-        assert(true);
+        //assert(true);
         //assertTrue(result.contains("\"longitud\" : 3"));
          
     
